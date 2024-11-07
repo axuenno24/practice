29b53d1586e90f9ab1a0d02e83536df236da6eb0
axuenno24 <qwertypqwertyp12345@gmail.com>
2024-11-07 16:30:14 +0700

    models

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,76 @@
-from django.db import models
+from tokenize import Triple
 
-# Create your models here.
+from django.db import models
+from django.db.models import SET_NULL
+from django.urls import reverse
+import uuid
+
+class Genre(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author',on_delete=models.SET_NULL, null=True )
+    summary = models.CharField(max_length=1000, help_text='Введите краткое описание книги')
+    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Выберите жанр этой книги")
+
+    def __str__(self):
+        """
+        Строка для представления объекта модели
+        """
+        return self.title
+
+    def get_absolute_url(self): # я говорю django, что для каждого объекта книги в админке или на веб-странице будет отображаться название книги
+        """
+        Возвращает URL-адрес для доступа к определенному экземпляру книги
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Уникальный идентификатор конкретной книги во всей библиотеке')
+    book = models.ForeignKey('Book', on_delete=SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Забронировать наличие')
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        """
+        Строка для представления объекта модели
+        """
+        return '%s (%s)' % (self.id, self.book.title)
+
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
