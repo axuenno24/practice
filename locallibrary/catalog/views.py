a8babc9ad658c4b47261b50e21beddec697a155a
axuenno24 <qwertypqwertyp12345@gmail.com>
2024-11-09 00:08:19 +0700

    list

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from  .models import Book, Genre, Author, BookInstance
+from django.views import generic
 
 def index(request):
     """Отображение главной страницы"""
@@ -13,3 +14,13 @@ def index(request):
                            'num_instances':num_instances,
                            'num_instances_available':num_instances_available,
                            'num_authors':num_authors},)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
